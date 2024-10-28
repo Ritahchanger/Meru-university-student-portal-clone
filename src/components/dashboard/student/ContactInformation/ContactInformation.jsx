0383c4abf4cd326a3980b9069318cda8aeaac542
Dennis Peter <peterdennis573@gmail.com>
2024-10-28 20:19:41 +0300

    Updates

--- src/components/dashboard/student/ContactInformation/ContactInformation.jsx
+++ src/components/dashboard/student/ContactInformation/ContactInformation.jsx
@@ -1,9 +1,7 @@
-
 const ContactInformation = () => {
   return (
     <div className="contact-information">
       <h3 className="subtitle">Contact Information</h3>
-      
     </div>
   );
 };
--- src/components/dashboard/student/PersonalInformation/PersonalInformation.jsx
+++ src/components/dashboard/student/PersonalInformation/PersonalInformation.jsx
@@ -1,4 +1,16 @@
 import ProfileIcon from "../../../../Assets/images/profileIcon.png";
+import "./PersonalInformation.css";
+const studentInformation = {
+  name: "MUNYAO PETER DENNIS",
+  regNo: "CT201/106094/21",
+  idNo: "39688058",
+  Campus: "MAIN",
+  gender: "Male",
+  dob: "29/10/2002",
+  county: "MAKUENI",
+  nationality: "KEN",
+  registeredUnits: [{ thisSem: 8 }, { lastSem: 43 }],
+};
 const PersonalInformation = () => {
   return (
     <div className="personal-information">
@@ -6,6 +18,32 @@ const PersonalInformation = () => {
       <div className="profile-icon-wrapper">
         <img src={ProfileIcon} alt="" />
       </div>
+      <ul>
+        <li>
+          <p>{studentInformation.name}</p>
+        </li>
+        <li>
+          <p>{studentInformation.regNo}</p>
+        </li>
+        <li>
+          <p>{studentInformation.idNo}</p>
+        </li>
+        <li>
+          <p>{studentInformation.Campus}</p>
+        </li>
+        <li>
+          <p>{studentInformation.gender}</p>
+        </li>
+        <li>
+          <p>{studentInformation.dob}</p>
+        </li>
+        <li>
+          <p>{studentInformation.county}</p>
+        </li>
+        <li>
+          <p>{studentInformation.nationality}</p>
+        </li>
+      </ul>
     </div>
   );
 };
