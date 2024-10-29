f34448585275eb902dad95ae579fbe51cdb9f429
Dennis Peter <peterdennis573@gmail.com>
2024-10-29 09:05:50 +0300

    Updates on the main section

--- src/components/dashboard/student/PersonalInformation/PersonalInformation.jsx
+++ src/components/dashboard/student/PersonalInformation/PersonalInformation.jsx
@@ -14,34 +14,35 @@ const studentInformation = {
 const PersonalInformation = () => {
   return (
     <div className="personal-information">
-      <h3 className="subtitle">Personal Information</h3>
+      <h3 className="subtitle font-semibold">Personal Information</h3>
       <div className="profile-icon-wrapper">
         <img src={ProfileIcon} alt="" />
+        <button className="text-xs-extra custom-orange py-[0.5rem] px-[1rem] mt-10">Update Information</button>
       </div>
       <ul>
         <li>
           <p>{studentInformation.name}</p>
         </li>
         <li>
-          <p>{studentInformation.regNo}</p>
+          <p><span className="font-semibold">Reg No:</span>{studentInformation.regNo}</p>
+        </li>
+        <li >
+          <p><span className="font-semibold">ID/Passport:</span>{studentInformation.idNo}</p>
         </li>
         <li>
-          <p>{studentInformation.idNo}</p>
+          <p><span className="font-semibold">Campus:</span>{studentInformation.Campus}</p>
         </li>
         <li>
-          <p>{studentInformation.Campus}</p>
+          <p><span className="font-semibold">Gender:</span>{studentInformation.gender}</p>
         </li>
         <li>
-          <p>{studentInformation.gender}</p>
+          <p><span className="font-semibold">DOB:</span>{studentInformation.dob}</p>
         </li>
         <li>
-          <p>{studentInformation.dob}</p>
+          <p><span className="font-semibold">County:</span>{studentInformation.county}</p>
         </li>
         <li>
-          <p>{studentInformation.county}</p>
-        </li>
-        <li>
-          <p>{studentInformation.nationality}</p>
+          <p><span className="font-semibold">Nationality:</span>{studentInformation.nationality}</p>
         </li>
       </ul>
     </div>
