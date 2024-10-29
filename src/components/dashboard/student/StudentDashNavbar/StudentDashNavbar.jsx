2c2a5d822429581407f52f2a802891b7036c0a7e
Dennis Peter <peterdennis573@gmail.com>
2024-10-29 19:54:48 +0300

    Updates on the navbar

--- src/components/dashboard/student/StudentDashNavbar/StudentDashNavbar.jsx
+++ src/components/dashboard/student/StudentDashNavbar/StudentDashNavbar.jsx
@@ -3,14 +3,25 @@ import { RiArrowDropDownLine } from "react-icons/ri";
 import "./StudentDashNavbar.css";
 import { FaEllipsisH } from "react-icons/fa";
 import { Fragment } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+
+import { FaTachometerAlt, FaSignOutAlt } from "react-icons/fa";
 import { toggleSidebar } from "../../../../store/features/SidebarSlice";
 const StudentDashNavbar = () => {
   const dispatch = useDispatch();
 
   const handleSidebarToggling = () => {
     dispatch(toggleSidebar());
+  };
+
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible((prev) => !prev);
   };
 
   return (
@@ -60,7 +71,7 @@ const StudentDashNavbar = () => {
 
           {/* mobile navigation */}
 
-          <div className="profile-card">
+          <div className="profile-card relative " onClick={toggleDropdown}>
             <div className="profile-icon mr-[1rem]">
               <img
                 src={AssetsExporter.profileIcon}
@@ -75,6 +86,31 @@ const StudentDashNavbar = () => {
                 <RiArrowDropDownLine />
               </span>
             </div>
+
+            {isDropdownVisible && (
+              <div className="absolute top-[100%] bg-white w-[250px] custom-shadow right-[2rem]">
+                <div className="triangle-up" />
+                <ul>
+                  <li className="flex items-center cursor-pointer hover:bg-green-500 hover:text-white transition-all h-[40px]">
+                    <Link
+                      to="/"
+                      className="flex p-2 items-end h-full text-xs-extra w-full"
+                    >
+                      {" "}
+                      <FaTachometerAlt className="mr-2" /> Dashboard
+                    </Link>
+                  </li>
+                  <li className="flex items-center p-2 cursor-pointer hover:bg-green-500 hover:text-white transition-all h-[40px]">
+                    <Link
+                      to="/auth/login"
+                      className="flex p-2 items-center h-full text-xs-extra w-full"
+                    >
+                      <FaSignOutAlt className="mr-2" /> Logout
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
