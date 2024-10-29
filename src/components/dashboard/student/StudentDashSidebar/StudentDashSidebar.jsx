989eb490b4ea3b8789b7b029369f37b51792d56b
Dennis Peter <peterdennis573@gmail.com>
2024-10-29 14:20:04 +0300

    Updates on the sidebar responsibility

--- src/components/dashboard/student/StudentDashSidebar/StudentDashSidebar.jsx
+++ src/components/dashboard/student/StudentDashSidebar/StudentDashSidebar.jsx
@@ -1,20 +1,45 @@
 import "./StudentDashSidebar.css";
 import { Link } from "react-router-dom";
 import sidebarData from "../../../../Data/SidebarData";
-import { useSelector } from "react-redux";
-const StudentDashSidebar = () => {
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { hideSidebar } from "../../../../store/features/SidebarSlice";
 
-  const isSidebarShown = useSelector((state) =>state.sidebar.isSidebarShown);
-  
+const StudentDashSidebar = () => {
+  const dispatch = useDispatch();
+  const isSidebarShown = useSelector((state) => state.sidebar.isSidebarShown);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+
+    if (screenWidth <= 820) {
+      dispatch(hideSidebar());
+    }
+
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const handleHideSidebar = () => {
+    if (screenWidth <= 820) {
+      dispatch(hideSidebar());
+    }
+  };
+
   return (
-    <div className={`sidebar ${isSidebarShown ? "active" : ''}`}>
+    <div className={`sidebar ${isSidebarShown ? "active" : ""}`}>
       <div className="sidebar-content">
         {sidebarData.map((item, index) => (
-          <div className="sidebar-card">
+          <div key={index} className="sidebar-card">
             <p className="card-title">{item.title}</p>
             <ul>
               {item.sidebarPages.map((insideItem, customID) => (
-                <li key={customID}>
+                <li key={customID} onClick={handleHideSidebar}>
                   <Link to={insideItem.path}>
                     <span>{insideItem.icon}</span>
                     {insideItem.subTitle}
