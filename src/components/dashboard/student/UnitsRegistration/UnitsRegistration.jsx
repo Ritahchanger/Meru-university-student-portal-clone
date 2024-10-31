baa5a1dc5609b44ec745843031e0ce1143b79fc7
Dennis Peter <peterdennis573@gmail.com>
2024-10-31 10:03:14 +0300

    Updates on the units registration

--- src/components/dashboard/student/UnitsRegistration/UnitsRegistration.jsx
+++ src/components/dashboard/student/UnitsRegistration/UnitsRegistration.jsx
@@ -9,6 +9,40 @@ const UnitsRegistration = () => {
           Having Trouble with Unit Registration? Click Here for to View How to
         </a>
       </div>
+
+      <p className="text-sm my-[1rem]">
+        Registration Semester: {""}{" "}
+        <span className="text-green-600">SEM1-2024/2025</span>
+      </p>
+
+      <p className="text-red-500 mb-[0.5rem] text-sm">
+        To register for unit(s), Choose "Registration Type" then Click on the
+        button "Get Units to Register". After you submit your unit(s), Confirm
+        then Complete registration by clicking button "Complete Registration"
+      </p>
+
+      <div className="flex items-center flex-col md:flex-row w-full md:w-auto gap-4 mb-1rem border-b border-neutral-300 pb-[2rem]">
+        <select
+          name="regType"
+          id="#"
+          className="bg-transparent text-sm border-b border-neutral-400 h-[40px] text-neutral-600 w-full md:w-1/2"
+        >
+          <option value="">--Registration Type--</option>
+          <option value="units">Units Registration</option>
+        </select>
+        <button className="text-sm py-[0.6rem] px-[1rem] bg-red-500 rounded-md text-white mt-[1rem] md:mt-0">
+          Get Units To Register
+        </button>
+      </div>
+      <button className="text-sm py-[0.6rem] px-[1rem] bg-red-500 rounded-md text-white mt-[1rem] ">
+        Complete Registration
+      </button>
+      <p className="my-2 text-neutral-400 text-sm">
+        Selected Units for Registration
+      </p>
+      <p className="p-[0.5rem] border border-neutral-300 text-xs-extra">
+        NO SELECTED UNITS FOUND
+      </p>
     </div>
   );
 };
