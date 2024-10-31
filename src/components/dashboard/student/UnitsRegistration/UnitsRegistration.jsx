129960d8dfb6362fd840af51618e0b41be2b2033
Dennis Peter <peterdennis573@gmail.com>
2024-10-31 09:35:04 +0300

    Updates on the units registration

--- src/components/dashboard/student/UnitsRegistration/UnitsRegistration.jsx
+++ src/components/dashboard/student/UnitsRegistration/UnitsRegistration.jsx
@@ -1,8 +1,16 @@
-
 const UnitsRegistration = () => {
   return (
-    <div className='p-[1rem]'>UnitsRegistration</div>
-  )
-}
+    <div className="p-[1rem]">
+      <h3 className="subtitle font-semibold text-sm underline">
+        UNITS REGISTRATION
+      </h3>
+      <div className="px-[1rem] py-[0.6rem] bg-blue-500 rounded-md">
+        <a href="#" className="text-xs-extra">
+          Having Trouble with Unit Registration? Click Here for to View How to
+        </a>
+      </div>
+    </div>
+  );
+};
 
-export default UnitsRegistration
+export default UnitsRegistration;
